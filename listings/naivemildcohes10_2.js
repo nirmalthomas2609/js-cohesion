c8c6f0b96732cb8a8e09b934d3fde8f0a4fbef65
Nirmal Thomas <nirmalthomas26@gmail.com>
2022-10-19 19:28:28 -0700

    Bad code / naive cohesion / listing 10.2

--- listings/naivemildcohes10_2.js
+++ listings/naivemildcohes10_2.js
@@ -1,22 +1,34 @@
 const fs = require("fs");
 
-class ReallyBadCohesion {
+class BadCohesion {
     loadProcessAndStore() {
+        var words = this.readWords();
+        words = this.sortWords(words);
+        this.storeWords(words); 
+    }
+
+    readWords() {
         var words = [];
         try {
             words = fs.readFileSync('./resources/words.txt').toString().split(" ");
+            return words;
         }
         catch(e){
             throw new Error("Error reading file - ", e);
         }
+    }
+
+    sortWords(words) {
         words.sort();
+        return words;
+    }
+
+    storeWords(words) {
         fs.writeFile('./resources/test/sorted.txt', words.join("\n"), (err) => {
             if (err) throw new Error("File write failed");
         });
-
-        return true;
     }
 }
 
-let temp = new ReallyBadCohesion();
+let temp = new BadCohesion();
 temp.loadProcessAndStore();
